03825a888416f025ac88edf26de5110938214876
Jose Hidago <jhidalgo521@gmail.com>
2020-06-25 14:58:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,10 +18,12 @@ def retrieval
 end
 
 def adding
-    
+ 
+ binding.pry   
+ 
  # shipping_manifest = {"whale bone corsets" => 5,
 #"porcelain vases" => 2, "oil paintings" => 3}
-  shipping_manifest
+  # shipping_manifest
  
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
