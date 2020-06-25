29ff65c8a5b60d57bb498e76c8040448fbb6db96
Jose Hidago <jhidalgo521@gmail.com>
2020-06-25 14:57:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,6 +21,7 @@ def adding
     
  # shipping_manifest = {"whale bone corsets" => 5,
 #"porcelain vases" => 2, "oil paintings" => 3}
+  shipping_manifest
  
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
