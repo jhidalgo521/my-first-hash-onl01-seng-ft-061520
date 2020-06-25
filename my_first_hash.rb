00dc18b1f525da086ee0885a29a9da4a24b4ebaf
Jose Hidago <jhidalgo521@gmail.com>
2020-06-25 14:55:33 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,8 +19,7 @@ end
 
 def adding
     
-    shipping_manifest = {"whale bone corsets" => 5,
-"porcelain vases" => 2, "oil paintings" => 3}
+  
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
   
@@ -28,3 +27,6 @@ def adding
   shipping_manifest
 end
 
+i didnt have to define shippinig_manifest in retrieval
+
+thats why i didnt want to do it this specific way, its redundant 
