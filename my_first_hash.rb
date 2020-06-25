de37567102e43f3c8c369eceeb0233f9ee38730c
Jose Hidago <jhidalgo521@gmail.com>
2020-06-25 15:02:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,8 +21,9 @@ def adding
  
  binding.pry   
  
- # shipping_manifest = {"whale bone corsets" => 5,
-#"porcelain vases" => 2, "oil paintings" => 3}
+ shipping_manifest = {"whale bone corsets" => 5,
+"porcelain vases" => 2, "oil paintings" => 3}
+
   # shipping_manifest
  
   shipping_manifest["muskets"] = 2
